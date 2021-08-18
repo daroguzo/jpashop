aa79efb10f8ed86eddce440a5be127c7bfeca1b5
daroguzo <kjw2122@naver.com>
2021-08-18 18:56:02 +0900

    domain config

--- src/main/java/jpabook/jpashop/MemberRepository.java
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -1,11 +1,10 @@
 package jpabook.jpashop;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import jpabook.jpashop.domain.Member;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
 
 @Repository
 public class MemberRepository {
