eafa77f0df132f04516bafd41ce29934e0404684
daroguzo <kjw2122@naver.com>
2021-08-20 11:21:59 +0900

    add OrderItem creation method

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,6 +25,17 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    // creation method
+    public static OrderItem createOrderItem(Item item, int orderPrice, int
+            count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
     // business logic
     // cancel order
     public void cancel() {
