973ab2edd8df41f1ef64bc524e7301cabf511ebd
daroguzo <kjw2122@naver.com>
2021-08-20 11:53:44 +0900

    modify access level

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,59 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.*;
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+    private final MemberRepository memberRepository;
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public Long order(Long memberId, Long itemId, int count) {
+
+        // lookup entity
+        Member member = memberRepository.findOne(memberId);
+        Item item = itemRepository.findOne(itemId);
+
+        // create delivery
+        Delivery delivery = new Delivery();
+        delivery.setAddress(member.getAddress());
+        delivery.setStatus(DeliveryStatus.READY);
+
+        // create orderItem
+        OrderItem orderItem = OrderItem.createOrderItem(item, item.getPrice(), count);
+
+        // create order
+        Order order = Order.createOrder(member, delivery, orderItem);
+
+        // save order
+        orderRepository.save(order);
+        return order.getId();
+    }
+
+    @Transactional
+    public void cancelOrder(Long orderId) {
+
+        // lookup order entity
+        Order order = orderRepository.findOne(orderId);
+
+        // cancel order
+        order.cancel();
+    }
+
+    // TODO
+//    public List<Order> findOrders(OrderSearch orderSearch) {
+//        return orderRepository.findAll(orderSearch);
+//    }
+}
